fdd3814ad35445c53cae0826a9270072fcd9b7ce
Demo <demo@example.com>
2026-07-29 19:19:47 +0300

    70% projects health + cleaner sector cards

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"82bd5b7209295a5b7ff8cae0df96e7870171e3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "623363846"
+    serviceWorkerVersion: "2902504698"
   }
 });
